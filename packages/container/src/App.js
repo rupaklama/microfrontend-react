861bec09fc96e3ab0a41034ffb8c5392dc83050d
pak <rupaklamadev@gmail.com>
2021-09-26 23:44:13 -0400

    updated file

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from './components/MarketingApp';
 const App = () => {
   return (
     <div>
-      <h1>Container App</h1>
+      <h1>Host App</h1>
       <hr />
       <MarketingApp />
     </div>
